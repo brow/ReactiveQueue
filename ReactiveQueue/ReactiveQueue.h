86bffa2d172250ffb560d222797bc0bc04ed55d9
Tom Brow <tom@tombrow.com>
2015-05-21 08:06:23 +0200

    fresh project

---
+++ ReactiveQueue/ReactiveQueue.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveQueue.h
+//  ReactiveQueue
+//
+//  Created by Tom Brow on 5/21/15.
+//  Copyright (c) 2015 Tom Brow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveQueue.
+FOUNDATION_EXPORT double ReactiveQueueVersionNumber;
+
+//! Project version string for ReactiveQueue.
+FOUNDATION_EXPORT const unsigned char ReactiveQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveQueue/PublicHeader.h>
+
+
